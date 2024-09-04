2060b7565aa8d23949f118c3793851e33e18ceff
BassemArfaoui <bassem.arfaoui@insat.ucar.tn>
2024-09-04 23:35:16 +0100

    finish notif time

--- src/components/parts/Notification.jsx
+++ src/components/parts/Notification.jsx
@@ -12,68 +12,113 @@ function Notification(props) {
     const SnippetTitle='Fibonaci function'
   return (
     <div>
-        {
-            props.type==='like' && 
-            <div className="d-flex align-items-center justify-content-center gap-3 mb-4">
-              <div className="noti-btn bg-primary text-light rounded-circle d-flex align-items-center justify-content-center px-2" style={{width:'40px',height:'40px'}}>
-                <ThumbUpAltIcon />
-              </div>
-              <div className='w-100'>
-                <p className="text-muted mb-0 fw-bold"><strong className='text-black'>{userName}</strong> liked your Snippet : <strong className='text-black'>{SnippetTitle}</strong></p>
-              </div>
-            </div>
-        }
+      {props.type === "like" && (
+        <div className="d-flex align-items-center justify-content-center gap-3 mb-4">
+          <div
+            className="noti-btn bg-primary text-light rounded-circle d-flex align-items-center justify-content-center px-2"
+            style={{ width: "40px", height: "40px" }}
+          >
+            <ThumbUpAltIcon />
+          </div>
+          <div className="w-100">
+            <p className="text-muted mb-0 fw-bold">
+              <strong className="text-black">{userName} </strong>
+              liked your Snippet :
+              <strong className="text-black"> {SnippetTitle}</strong>
+              <span className="small ms-2"> (2min)</span>
+            </p>
+          </div>
+        </div>
+      )}
 
-        {
-            props.type==='dislike' && 
-            <div className="d-flex align-items-center justify-content-center gap-3 mb-4">
-              <div className="noti-btn bg-danger text-light rounded-circle d-flex align-items-center justify-content-center px-2" style={{width:'40px',height:'40px'}}>
-                <ThumbDownAltIcon />
-              </div>
-              <div className='w-100'>
-                <p className="text-muted mb-0 fw-bold"><strong className='text-black'>{userName}</strong> disliked your Snippet : <strong className='text-black'>{SnippetTitle}</strong></p>
-              </div>
-            </div>
-        }
+      {props.type === "dislike" && (
+        <div className="d-flex align-items-center justify-content-center gap-3 mb-4">
+          <div
+            className="noti-btn bg-danger text-light rounded-circle d-flex align-items-center justify-content-center px-2"
+            style={{ width: "40px", height: "40px" }}
+          >
+            <ThumbDownAltIcon />
+          </div>
+          <div className="w-100">
+            <p className="text-muted mb-0 fw-bold">
+              <strong className="text-black">{userName} </strong>
+              disliked yourSnippet :
+              <strong className="text-black"> {SnippetTitle}</strong>
+              <span className="small ms-2"> (2min)</span>
+            </p>
+          </div>
+        </div>
+      )}
 
-        {
-            props.type==='comment' && 
-            <div className="d-flex align-items-center justify-content-center gap-3 mb-4">
-              <div className="noti-btn text-light rounded-circle d-flex align-items-center justify-content-center px-2" style={{width:'40px',height:'40px',backgroundColor:'rgb(255, 98, 0)'}}>
-                <CommentIcon />
-              </div>
-              <div className='w-100'>
-                <p className="text-muted mb-0 fw-bold"><strong className='text-black'>{userName}</strong> Commented on your Snippet : <strong className='text-black'>{SnippetTitle}</strong></p>
-              </div>
-            </div>
-        }
+      {props.type === "comment" && (
+        <div className="d-flex align-items-center justify-content-center gap-3 mb-4">
+          <div
+            className="noti-btn text-light rounded-circle d-flex align-items-center justify-content-center px-2"
+            style={{
+              width: "40px",
+              height: "40px",
+              backgroundColor: "rgb(255, 98, 0)",
+            }}
+          >
+            <CommentIcon />
+          </div>
+          <div className="w-100">
+            <p className="text-muted mb-0 fw-bold">
+              <strong className="text-black">{userName}</strong> Commented on
+              your Snippet :
+              <strong className="text-black"> {SnippetTitle}</strong>
+              <span className="small ms-2"> (2min)</span>
+            </p>
+          </div>
+        </div>
+      )}
 
-        
-        {
-            props.type==='intrested' && 
-            <div className="d-flex align-items-center justify-content-center gap-3 mb-4">
-              <div className="noti-btn text-light bg-success rounded-circle d-flex align-items-center justify-content-center px-2" style={{width:'40px',height:'40px',backgroundColor:'rgb(255, 98, 0)'}}>
-                <StarIcon />
-              </div>
-              <div className='w-100'>
-                <p className="text-muted mb-0 fw-bold"><strong className='text-black'>{userName}</strong> is now intrested in your Snippets</p>
-              </div>
-            </div>
-        }
+      {props.type === "intrested" && (
+        <div className="d-flex align-items-center justify-content-center gap-3 mb-4">
+          <div
+            className="noti-btn text-light bg-success rounded-circle d-flex align-items-center justify-content-center px-2"
+            style={{
+              width: "40px",
+              height: "40px",
+              backgroundColor: "rgb(255, 98, 0)",
+            }}
+          >
+            <StarIcon />
+          </div>
+          <div className="w-100">
+            <p className="text-muted mb-0 fw-bold">
+              <strong className="text-black">{userName} </strong>
+               is now intrested in your Snippets
+               <span className='small ms-2'> (2min)</span>
+            </p>
+          </div>
+        </div>
+      )}
 
-        {
-            props.type==='share' && 
-            <div className="d-flex align-items-center justify-content-center gap-3 mb-4">
-              <div className="noti-btn text-light bg-secondary rounded-circle d-flex align-items-center justify-content-center px-2" style={{width:'40px',height:'40px',backgroundColor:'rgb(255, 98, 0)'}}>
-                <ShareIcon  />
-              </div>
-              <div className='w-100'>
-                <p className="text-muted mb-0 fw-bold"><strong className='text-black'>{userName}</strong> shared your Snippet : <strong className='text-black'>{SnippetTitle}</strong></p>
-              </div>
-            </div>
-        }
+      {props.type === "share" && (
+        <div className="d-flex align-items-center justify-content-center gap-3 mb-4">
+          <div
+            className="noti-btn text-light bg-secondary rounded-circle d-flex align-items-center justify-content-center px-2"
+            style={{
+              width: "40px",
+              height: "40px",
+              backgroundColor: "rgb(255, 98, 0)",
+            }}
+          >
+            <ShareIcon />
+          </div>
+          <div className="w-100">
+            <p className="text-muted mb-0 fw-bold">
+              <strong className="text-black">{userName} </strong>
+               shared your Snippet : 
+               <strong className="text-black"> {SnippetTitle}</strong>
+               <span className='small ms-2'> (2min)</span>
+            </p>
+          </div>
+        </div>
+      )}
     </div>
-  )
+  );
 }
 
 export default Notification
