498c42110617c0e76865f4606b67ae4a60418b55
BassemArfaoui <bassem.arfaoui@insat.ucar.tn>
2024-09-23 18:57:03 +0100

    add filter result compo

---
+++ src/components/saved/LocalPostsFilter.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './styles/filter-result.css'
+
+
+
+function LocalPostsFilter() {
+  return (
+    <div>LocalPostsFilter</div>
+  )
+}
+
+export default LocalPostsFilter
---
+++ src/components/saved/SavedPostsFilter.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './styles/filter-result.css'
+
+
+
+function SavedPostsFilter(props) {
+  return (
+    <div>
+        saved posts filter
+    </div>
+  )
+}
+
+export default SavedPostsFilter
