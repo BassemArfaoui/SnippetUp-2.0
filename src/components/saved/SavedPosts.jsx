a18de1ce48130742b4e48ba9b40c7ea65458c78e
BassemArfaoui <bassem.arfaoui@insat.ucar.tn>
2024-09-23 18:14:47 +0100

    extract the filter modal to the saved page compo

--- src/components/saved/SavedPosts.jsx
+++ src/components/saved/SavedPosts.jsx
@@ -3,8 +3,6 @@ import axios from 'axios';
 import Post from '../parts/Post';
 import Spinner from '../tools/Spinner';
 import { Modal, Box, IconButton } from '@mui/material';
-import CloseIcon from '@mui/icons-material/Close';
-import DoneRoundedIcon from '@mui/icons-material/DoneRounded';
 import './styles/saves.css';
 import './styles/filter.css'
 import MoreHorizIcon from '@mui/icons-material/MoreHoriz';
@@ -12,9 +10,8 @@ import TuneIcon from '@mui/icons-material/Tune';
 import AppsIcon from '@mui/icons-material/Apps';
 import CustomTooltip from '../tools/CustomTooltip';
 import { notify } from '../tools/CustomToaster';
-import SpinnerSpan from '../tools/SpinnerSpan';
 
-function SavedPosts({ setShowChoice }) {
+function SavedPosts(props) {
   const userId = 1;
   const limit = 10;
   const savedPostsRef = useRef(null);
@@ -26,11 +23,7 @@ function SavedPosts({ setShowChoice }) {
   const [initialLoading, setInitialLoading] = useState(true);
   const [hasMore, setHasMore] = useState(true);
   const [showOptions, setShowOptions] = useState(false);
-  const [filterModalOpen, setFilterModalOpen] = useState(false);
-  const [filterLanguage, setFilterLanguage] = useState('');
-  const [filterTitle, setFilterTitle] = useState('');
-  const [filterContent, setFilterContent] = useState('');
-  const [filterLoading,setFilterloading]=useState(false);
+
 
   const loadSavedPosts = async () => {
     try {
@@ -55,21 +48,8 @@ function SavedPosts({ setShowChoice }) {
     setShowOptions(prev => !prev);
   };
 
-  const handleFilterOpen = () => {
-    setFilterModalOpen(true);
 
-  };
 
-  const handleFilterClose = () => {
-    setFilterLanguage('');
-    setFilterTitle('');
-    setFilterContent('');
-    setFilterModalOpen(false);
-  };
-
-  const applyFilters = () => {
-   setFilterloading(true);
-  };
 
 
   
@@ -86,7 +66,7 @@ function SavedPosts({ setShowChoice }) {
       const scrollDifference = Math.abs(scrollTop - lastScrollTop.current);
 
       if (scrollDifference >= scrollThreshold) {
-        setShowChoice(scrollTop < lastScrollTop.current);
+        props.setShowChoice(scrollTop < lastScrollTop.current);
         lastScrollTop.current = scrollTop;
       }
 
@@ -105,7 +85,7 @@ function SavedPosts({ setShowChoice }) {
         savedPostsElement.removeEventListener('scroll', handleScroll);
       }
     };
-  }, [loading, setShowChoice]);
+  }, [loading, props.setShowChoice]);
 
   return (
     <div
@@ -127,7 +107,7 @@ function SavedPosts({ setShowChoice }) {
               <IconButton
                 className="text-dark bg-warning"
                 variant="contained"
-                onClick={handleFilterOpen} 
+                onClick={props.handleFilterOpen} 
               >
                 <TuneIcon fontSize="large" className="text-dark" />
               </IconButton>
@@ -147,92 +127,7 @@ function SavedPosts({ setShowChoice }) {
 
 
 
-      <Modal
-        open={filterModalOpen}
-        onClose={handleFilterClose}
-        aria-labelledby="filter-modal-title"
-        aria-describedby="filter-modal-description"
-      >
-        <Box
-          sx={{
-            backgroundColor: '#333',
-            width: '60%',
-            height: '60vh',
-            paddingX: '70px',
-            paddingY: '20px',
-            overflowY: 'auto',
-            borderRadius: '20px',
-            color: '#fff', 
-            top: '50%',
-            left: '50%',
-            transform: 'translate(-50%, -50%)', 
-            position: 'relative'
-          }}
-        >
-  
-
-            <IconButton
-              aria-label="close"
-              onClick={handleFilterClose}
-              sx={{
-                position: 'absolute',
-                top: '10px',
-                right: '10px',
-                color: 'white'
-              }}
-            >
-              <CloseIcon className='fs-2'/>
-            </IconButton>
-
-        <h2 id="filter-modal-title" className='text-center mb-4 mt-2 fw-bold text-warning'>Filter Saved Posts</h2>
-
-        <div className="filters-buttons h-75 w-100 d-flex flex-column justify-content-center align-items-center mt-3">
-          <form action="" method='POST'>
-            <div className='d-flex flex-column gap-3'>
-                <div className='d-flex flex-column flex-md-row gap-3'>
-                  <input
-                    className='filter-input form-control'
-                    placeholder='Title'
-                    value={filterTitle}
-                    onChange={(e) => setFilterTitle(e.target.value)}
-                  />
-                  <input
-                    value={filterLanguage}
-                    className='filter-input form-control'
-                    placeholder='Language'
-                    onChange={(e) => setFilterLanguage(e.target.value)}
-                  />
-                </div>
-              <textarea
-                className='filter-input form-control'
-                value={filterContent}
-                placeholder='Content'
-                onChange={(e) => setFilterContent(e.target.value)}
-              ></textarea>
-            </div>
-          </form>
-
-
-          <div className="d-flex justify-content-center mt-5">
-            <CustomTooltip title='Apply Filters' placement='top'>
-              <IconButton
-                aria-label="Scroll to End"
-                className="mx-4 mt-0 bg-warning" 
-                style={{ backgroundColor: '#f8f9fa' }} >
-
-               {!filterLoading ? 
-               <DoneRoundedIcon fontSize="large" className="text-dark fw-bolder" onClick={applyFilters}/> : 
-               <SpinnerSpan/>
-               }
-
-              </IconButton>
-            </CustomTooltip>
-          </div>
-        </div>
-
-        </Box>
-      </Modal>
-
+   
 
 
       {initialLoading ? (
--- src/pages/SavedPage.jsx
+++ src/pages/SavedPage.jsx
@@ -5,6 +5,12 @@ import SavedPosts from '../components/saved/SavedPosts';
 import SavedLocal from '../components/saved/SavedLocal';
 import PostsSearch from '../components/saved/PostsSearch';
 import LocalSearch from '../components/saved/LocalSearch';
+import CloseIcon from '@mui/icons-material/Close';
+import { Modal, Box, IconButton } from '@mui/material';
+import DoneRoundedIcon from '@mui/icons-material/DoneRounded';
+import CustomTooltip from '../components/tools/CustomTooltip';
+import SpinnerSpan from '../components/tools/SpinnerSpan'
+
 
 
 
@@ -16,6 +22,32 @@ function SavedPage() {
   const [postsSearch , setPostsSearch]=useState('');
   const [localSearch, setLocalSearch]=useState('');
 
+  const [filterModalOpen, setFilterModalOpen] = useState(false);
+  const [filterLanguage, setFilterLanguage] = useState('');
+  const [filterTitle, setFilterTitle] = useState('');
+  const [filterContent, setFilterContent] = useState('');
+  const [filterLoading,setFilterloading]=useState(false);
+
+
+
+  const handleFilterClose = () => {
+    setFilterLanguage('');
+    setFilterTitle('');
+    setFilterContent('');
+    setFilterModalOpen(false);
+  };
+
+
+  const handleFilterOpen = () => {
+    setFilterModalOpen(true);
+
+  };
+
+
+  const applyFilters = () => {
+    setFilterloading(true);
+   };
+ 
   
 
   return (
@@ -30,7 +62,7 @@ function SavedPage() {
         {
           isSearching === 'none' ?
           <>{
-            choice==='posts' ? <SavedPosts setShowChoice={setShowChoice}/>
+            choice==='posts' ? <SavedPosts setShowChoice={setShowChoice} handleFilterOpen={handleFilterOpen} handleFilterClose={handleFilterClose} />
             : <SavedLocal  setShowChoice={setShowChoice}/>
             } </> :
            <>
@@ -40,6 +72,93 @@ function SavedPage() {
             }
            </>
         }
+
+
+        <Modal
+        open={filterModalOpen}
+        onClose={handleFilterClose}
+        aria-labelledby="filter-modal-title"
+        aria-describedby="filter-modal-description"
+      >
+        <Box
+          sx={{
+            backgroundColor: '#333',
+            width: '60%',
+            height: '60vh',
+            paddingX: '70px',
+            paddingY: '20px',
+            overflowY: 'auto',
+            borderRadius: '20px',
+            color: '#fff', 
+            top: '50%',
+            left: '50%',
+            transform: 'translate(-50%, -50%)', 
+            position: 'relative'
+          }}
+        >
+  
+
+            <IconButton
+              aria-label="close"
+              onClick={handleFilterClose}
+              sx={{
+                position: 'absolute',
+                top: '10px',
+                right: '10px',
+                color: 'white'
+              }}
+            >
+              <CloseIcon className='fs-2'/>
+            </IconButton>
+
+        <h2 id="filter-modal-title" className='text-center mb-4 mt-2 fw-bold text-warning'>Filter Saved Posts</h2>
+
+        <div className="filters-buttons h-75 w-100 d-flex flex-column justify-content-center align-items-center mt-3">
+          <form action="" method='POST'>
+            <div className='d-flex flex-column gap-3'>
+                <div className='d-flex flex-column flex-md-row gap-3'>
+                  <input
+                    className='filter-input form-control'
+                    placeholder='Title'
+                    value={filterTitle}
+                    onChange={(e) => setFilterTitle(e.target.value)}
+                  />
+                  <input
+                    value={filterLanguage}
+                    className='filter-input form-control'
+                    placeholder='Language'
+                    onChange={(e) => setFilterLanguage(e.target.value)}
+                  />
+                </div>
+              <textarea
+                className='filter-input form-control'
+                value={filterContent}
+                placeholder='Content'
+                onChange={(e) => setFilterContent(e.target.value)}
+              ></textarea>
+            </div>
+          </form>
+
+
+          <div className="d-flex justify-content-center mt-5">
+            <CustomTooltip title='Apply Filters' placement='top'>
+              <IconButton
+                aria-label="Scroll to End"
+                className="mx-4 mt-0 bg-warning" 
+                style={{ backgroundColor: '#f8f9fa' }} >
+
+               {!filterLoading ? 
+               <DoneRoundedIcon fontSize="large" className="text-dark fw-bolder" onClick={applyFilters}/> : 
+               <SpinnerSpan/>
+               }
+
+              </IconButton>
+            </CustomTooltip>
+          </div>
+        </div>
+
+        </Box>
+      </Modal>
     </div>
   )
 }
