e730e75d19e0faa097830d50672e58ccb007f830
BassemArfaoui <bassem.arfaoui@insat.ucar.tn>
2024-10-10 17:08:00 +0100

    edit saved local error ui

--- src/components/saved/local-snippets/SavedLocal.jsx
+++ src/components/saved/local-snippets/SavedLocal.jsx
@@ -5,23 +5,32 @@ import Snippet from './Snippet';
 import '../styles/saves.css';
 import SpinnerSpan from '../../tools/SpinnerSpan'
 import Spinner from '../../tools/Spinner';
+import { notify } from '../../tools/CustomToaster';
 
 function SavedLocal({ setShowChoice }) {
-  const userId=1;
+  const userId = 1;
   const savedLocalRef = useRef(null);
   const lastScrollTop = useRef(0);
   const scrollThreshold = 40;
 
+
+
+
   // Fetch snippets using React Query
   const fetchSnippets = async ({ pageParam = 1 }) => {
-    const response = await axios.get(`http://localhost:4000/${userId}/snippets`, {
-      params: {
-        page: pageParam,
-        limit: 10,
-      },
-    });
+    const response = await axios.get(
+      `http://localhost:4000/${userId}/snippets`,
+      {
+        params: {
+          page: pageParam,
+          limit: 10,
+        },
+      }
+    );
     return response.data;
   };
+
+
 
   const {
     data,
@@ -32,13 +41,19 @@ function SavedLocal({ setShowChoice }) {
     isError,
     error,
   } = useInfiniteQuery({
-    queryKey: ['snippets'],
+    queryKey: ["snippets"],
     queryFn: fetchSnippets,
     getNextPageParam: (lastPage, pages) => {
       return lastPage.totalPages > pages.length ? pages.length + 1 : undefined;
     },
     refetchOnWindowFocus: true,
   });
+
+  useEffect(() => {
+    if (isError && error) {
+      notify(`Error loading  Snippets`);
+    }
+  }, [isError, error]);
 
   // Handle scrolling to trigger loading more snippets
   useEffect(() => {
@@ -59,7 +74,7 @@ function SavedLocal({ setShowChoice }) {
       // Load more snippets when scrolled to bottom
       if (
         savedLocalRef.current.scrollHeight - scrollTop <=
-        savedLocalRef.current.clientHeight + 100 &&
+          savedLocalRef.current.clientHeight + 100 &&
         hasNextPage &&
         !isFetchingNextPage
       ) {
@@ -70,28 +85,32 @@ function SavedLocal({ setShowChoice }) {
     const savedLocalElement = savedLocalRef.current;
     // Ensure the ref is valid before adding the event listener
     if (savedLocalElement) {
-      savedLocalElement.addEventListener('scroll', handleScroll);
+      savedLocalElement.addEventListener("scroll", handleScroll);
     }
 
     return () => {
       if (savedLocalElement) {
-        savedLocalElement.removeEventListener('scroll', handleScroll);
+        savedLocalElement.removeEventListener("scroll", handleScroll);
       }
     };
   }, [setShowChoice, fetchNextPage, hasNextPage, isFetchingNextPage]);
 
   // Loading state
   if (isLoading) {
-    return <Spinner />;
+    return (
+      <div style={{height:'85vh'}}>
+        <Spinner />
+      </div>
+    );
   }
 
   // Error state
   if (isError) {
-    return <div>Error: {error.message}</div>;
+    return <div  style={{height:'85vh'}} className='fw-bolder text-danger fs-2 d-flex justify-content-center align-items-center'>Couldn't Load Snippets 😿 !</div>;
   }
 
   return (
-    <div className='local-saves d-flex flex-column gap-3' ref={savedLocalRef}>
+    <div className="local-saves d-flex flex-column gap-3" ref={savedLocalRef}>
       {data.pages.map((page) =>
         page.snippets.map((snippet) => (
           <Snippet
